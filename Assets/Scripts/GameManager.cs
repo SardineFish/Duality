ed887e6c1a6831d48b2a76c3505d0f0a4a59332c
SardineFish <sardine@live.cn>
2022-01-23 11:56:44 +0800

    Replace diamond with fire and fix spawn position
    Signed-off-by: SardineFish <sardine@live.cn>

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Duality;
 using SardineFish.Utils;
 using UnityEngine;
 
@@ -9,11 +10,16 @@ public class GameManager : RuntimeSingleton<GameManager>
     [SerializeField] private int m_currentLevel;
     [SerializeField] private List<Vector2> m_stageDiamondPos;
     [SerializeField] private GameObject m_diamondPrefab;
+    [SerializeField] private int m_diamondGravityDir;
 
     public void CheckForDiamond(Vector2 pos)
     {
         if (pos == m_stageDiamondPos[m_currentLevel])
-            Instantiate(m_diamondPrefab, pos, Quaternion.identity);
+        {
+            var obj = Instantiate(m_diamondPrefab, pos + new Vector2(0.5f, 0.5f), Quaternion.identity);
+            var fire = obj.GetComponent<Fire>();
+            fire.GravityDirection = m_diamondGravityDir;
+        }
     }
 
     public void LoadNextLevel()
@@ -26,7 +32,7 @@ public class GameManager : RuntimeSingleton<GameManager>
         Gizmos.color = Color.red;
         foreach (var pos     in m_stageDiamondPos)
         {
-            Gizmos.DrawWireSphere(pos.ToVector3(0), 0.5f);
+            Gizmos.DrawWireSphere(pos.ToVector3(0) + new Vector3(0.5f, 0.5f, 0), 0.5f);
         }
     }
 }
