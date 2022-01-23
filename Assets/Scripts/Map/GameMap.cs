1bcb86899effc2d410e27b4e2bf7d47d1f7bf881
SardineFish <sardine@live.cn>
2022-01-23 13:16:36 +0800

    Add white tile collision
    Signed-off-by: SardineFish <sardine@live.cn>

--- Assets/Scripts/Map/GameMap.cs
+++ Assets/Scripts/Map/GameMap.cs
@@ -1,21 +1,63 @@
-﻿using SardineFish.Utils;
+﻿using System.Collections.Generic;
+using SardineFish.Utils;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace Duality
 {
     [RequireComponent(typeof(Tilemap))]
-    public class GameMap : RuntimeSingleton<GameMap>
+    public class GameMap : RuntimeSingleton<GameMap>, ICustomEditorEX
     {
         
         private Tilemap _tilemap;
         [SerializeField] private Tilemap _collisionTile;
+        [SerializeField] private Tilemap _whiteCollisionTile;
         [SerializeField] private TileBase _backTile;
+        [SerializeField] private TileBase _whiteTile;
 
         protected override void Awake()
         {
             base.Awake();
             _tilemap = GetComponent<Tilemap>();
+
+        }
+
+        [EditorButton]
+        void GenCollider()
+        {
+            _whiteCollisionTile.ClearAllTiles();
+            _collisionTile.ClearAllTiles();
+            _tilemap = GetComponent<Tilemap>();
+            var list = new List<Vector2Int>();
+            list.Add(Vector2Int.zero);
+
+            void AddPoint(Vector2Int point)
+            {
+                if (list.Contains(point))
+                    return;
+                list.Add(point);
+            }
+            for (var i = 0; i < list.Count; ++i)
+            {
+                var pos = list[i].ToVector3Int();
+                var tile = _tilemap.GetTile(pos);
+                if(!tile)
+                    continue;
+                
+                if (tile == _whiteTile)
+                {
+                    _whiteCollisionTile.SetTile(pos, _whiteTile);
+                }
+                else if (tile == _backTile)
+                {
+                    _collisionTile.SetTile(pos, _backTile);
+                }
+
+                AddPoint(list[i] + Vector2Int.up);
+                AddPoint(list[i] + Vector2Int.down);
+                AddPoint(list[i] + Vector2Int.left);
+                AddPoint(list[i] + Vector2Int.right);
+            }
         }
 
 
